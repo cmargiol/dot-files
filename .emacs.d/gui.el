b4791cf1feb9e34e8c5d45769d960bd5d20c9db1
harry <harry@thoughtmachine.net>
2017-04-19 20:30:11 +0100

    Spacemacs UI

--- .emacs.d/gui.el
+++ .emacs.d/gui.el
@@ -10,24 +10,28 @@
 ;;   :init (load-theme 'tango t))
 
 ;; Alex's theme
-(use-package zenburn-theme
-  :ensure t
-  :init (load-theme 'zenburn t)
-  :config
-  (zenburn-with-color-variables
-    (custom-theme-set-faces
-     'zenburn
-     `(mode-line
-       ((,class (:foreground "black" :background "#f9b593" :box nil))
-        (t :inverse-video t)))
-     `(mode-line-inactive
-       ((t (:foreground ,zenburn-green-1 :background ,zenburn-bg-05 :box nil))))
-     `(mode-line-buffer-id ((t (:foreground "black" :weight bold))))
-     `(powerline-active1
-       ((t (:foreground ,zenburn-green-1 :background ,zenburn-bg-05
-                        :inherit mode-line))))
-     `(powerline-active2 ((t (:background ,zenburn-bg+2 :inherit mode-line)))))))
+;; (use-package zenburn-theme
+;;   :ensure t
+;;   :init (load-theme 'zenburn t)
+;;   :config
+;;   (zenburn-with-color-variables
+;;     (custom-theme-set-faces
+;;      'zenburn
+;;      `(mode-line
+;;        ((,class (:foreground "black" :background "#f9b593" :box nil))
+;;         (t :inverse-video t)))
+;;      `(mode-line-inactive
+;;        ((t (:foreground ,zenburn-green-1 :background ,zenburn-bg-05 :box nil))))
+;;      `(mode-line-buffer-id ((t (:foreground "black" :weight bold))))
+;;      `(powerline-active1
+;;        ((t (:foreground ,zenburn-green-1 :background ,zenburn-bg-05
+;;                         :inherit mode-line))))
+;;      `(powerline-active2 ((t (:background ,zenburn-bg+2 :inherit mode-line)))))))
 
+(use-package spacemacs-theme
+  :ensure t
+  :defer t
+  :init (load-theme 'spacemacs-dark t))
 
 (if (eq system-type 'darwin) ;; on the mac use a larger font (larger resolution)
     (set-face-attribute 'default nil
